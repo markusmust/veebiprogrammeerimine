15e52990ac08de910b4287ba6512d7f350653b43
Markus Must <markusmust@users.noreplay.github.com>
2018-09-11 14:22:27 +0300

    lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Markus Must, õppetöö</title>
+  </head>
+  <body>
+  <h1>Markus Must</h1>
+  <p>See on minu <a href="https://www.tlu.ee/">TLÜ</a> õppetöö raames  valminud veebileht ja ei oma mingisugust, mõtestatud või muul moel väärtuslikku sisu</p>
+  <img src="../tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone">
+  <p>Minu sõber teeb ka <a href="../../~jaagala" target=blank>veebi</a></p>
+ 
+ 
+  </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
